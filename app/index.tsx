1bd27afbbb92ee3cd06269ca1e7b0909c8e2a181
David Bustamante <66386035+D4v13B@users.noreply.github.com>
2025-01-12 18:25:59 -0500

    first commit settings

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+const HomeLayout = () => {
+   return (
+      <View>
+         <Text className="">HomeLayout</Text>
+      </View>
+   )
+}
+export default HomeLayout
